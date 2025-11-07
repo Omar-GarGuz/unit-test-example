fd9b4eedc70c9fcbb6d0571c4c0e20ed6d09ad04
Omar-GarGuz <omar.garcia447@pascualbravo.edu.co>
2025-11-07 03:23:02 +0000

    [database/dummyDataset.js]: remove duplicates, add default values [calculators/environment.js]: clean exports [api/items/network.js]: fix dummyDataset access, create all calcs [test/env_test.js]: add test suite with descriptions, states and groups

--- api/items/network.js
+++ api/items/network.js
@@ -1,8 +1,11 @@
-const { Router } = require('express');
-const response = require('../../network/response')
+const {Router} = require('express');
+const response = require('../../network/response');
+const dummyDataset = require('../database/dummyDataset');
 const router = Router();
 const ctrl = require('./index');
-const {tiMonth,fuelEnergySelector, 
+const {
+    tiMonth,
+    fuelEnergySelector, 
     electricalConsumption,
     costElectricalKM,
     combustionConsumption,
@@ -21,61 +24,64 @@ const {tiMonth,fuelEnergySelector,
     energyH2LowPresure,
     energyConsumed,
     hydrogenMass,
-    litersRequired} = require('../../calculators/environment')
-const tableInjected = 'my_table'
- 
+    litersRequired
+} = require('../../calculators/environment');
+const tableInjected = 'my_table';
  
 router.get('/environment/:ipc/:fes', async (req, res) => {
     try {
-        let list={}
- 
-        const ipc=tiMonth(parseFloat(req.params.ipc))
-        const fes = fuelEnergySelector(req.params.fes)
-        // const electrical_consumption =  electricalConsumption(8.14, 14.7)
-        // const cost_electrical_km= costElectricalKM(electrical_consumption,978.81)
-        // const combustion_Consumption=combustionConsumption(electrical_consumption)
-        // const fuel_consumption=fuelConsumption()
-        // const fuel_Efficiency=fuelEfficiency()  
-        // const fuel_cost_KM= fuelCostKm()
-        // const energy_KM=energyKm()
-        // const emision_KM=emisionKm()
-        // const saved_energy=savedEnergy()
-        // const avoided_emissions=avoidedEmissions()
-        // const annual_savings=annualSavings()
-        // const monthly_savings=monthlySavings()
-        // const young_tree=youngTree()
-        // const old_tree=oldTree()
-        // const energy_H2_Cylinders=energyH2Cylinders()
-        // const energy_H2_Low_Presure=energyH2LowPresure()
-        // const energy_consumed=energyConsumed()
-        // const hydrogen_mass=hydrogenMass()
-        // const liters_required=litersRequired()
-        list["It_month"]=ipc
-        list["Fuel_energy_selector"]=fes
-        // list["Electrical_consumption"]=electrical_consumption
-        // list["Cost_electrical_KM"]=cost_electrical_km
-        // list["Combustion_Consumption"]=combustion_Consumption
-        // list["Fuel_Consumption"]=fuel_consumption
-        // list["Fuel_Efficiency"]=fuel_Efficiency
-        // list["Fuel_cost_KM"]=fuel_cost_KM
-        // list["Energy_KM"]=energy_KM
-        // list["Emision_KM"]=emision_KM
-        // list["Saved_energy"]=saved_energy
-        // list["Avoided_emissions"]=avoided_emissions
-        // list["Monthly_savings"]=monthly_savings
-        // list["Annual_savings"]=annual_savings
-        // list["Young_tree"]=young_tree
-        // list["Old_tree"]=old_tree
-        // list["Energy_H2_Cylinders"]=energy_H2_Cylinders
-        // list["Energy_H2_Low_Presure"]=energy_H2_Low_Presure
-        // list["Energy_consumed"]=energy_consumed
-        // list["Hydrogen_mass"]=hydrogen_mass
-        // list["Liters_required"]=liters_required
- 
-        response.success(req,res,list,200)  
+        let list = {};
+
+        const ipc = tiMonth(parseFloat(req.params.ipc));
+        const fes = fuelEnergySelector(req.params.fes);
+
+        const electrical_consumption = electricalConsumption(dummyDataset[nominal_energy], dummyDataset[autonomy_nominal]);
+        const cost_electrical_km = costElectricalKM(electrical_consumption, dummyDataset[energy_price]);
+        const combustion_Consumption = combustionConsumption(electrical_consumption);
+        const fuel_consumption = fuelConsumption(combustion_Consumption, fes[fuel_energy]);
+        const fuel_Efficiency = fuelEfficiency(fuel_consumption);
+        const fuel_cost_KM = fuelCostKm(fes[fuel_price], fuel_consumption);
+        const energy_KM = energyKm(combustion_Consumption);
+        const emision_KM = emisionKm(fes[emision_factor], energy_KM);
+        const saved_energy = savedEnergy(combustion_Consumption, electrical_consumption, dummyDataset[annual_use]);
+        const avoided_emissions = avoidedEmissions(emision_KM, dummyDataset[annual_use]);
+        const monthly_savings = monthlySavings(fuel_cost_KM, cost_electrical_km, dummyDataset[annual_use]);
+        const annual_savings = annualSavings(monthly_savings, ipc);
+        const young_tree = await youngTree(avoided_emissions);
+        const old_tree = await oldTree(avoided_emissions);
+        const energy_H2_Cylinders = await energyH2Cylinders(dummyDataset[nominal_energy]);
+        const energy_H2_Low_Presure = await energyH2LowPresure(energy_H2_Cylinders);
+        const energy_consumed = await energyConsumed(energy_H2_Low_Presure);
+        const hydrogen_mass = await hydrogenMass(energy_H2_Low_Presure);
+        const liters_required = await litersRequired(hydrogen_mass);
+
+        // populate list
+        list["It_month"] = ipc;
+        list["Fuel_energy_selector"] = fes;
+        list["Electrical_consumption"] = electrical_consumption;
+        list["Cost_electrical_KM"] = cost_electrical_km;
+        list["Combustion_Consumption"] = combustion_Consumption;
+        list["Fuel_Consumption"] = fuel_consumption;
+        list["Fuel_Efficiency"] = fuel_Efficiency;
+        list["Fuel_cost_KM"] = fuel_cost_KM;
+        list["Energy_KM"] = energy_KM;
+        list["Emision_KM"] = emision_KM;
+        list["Saved_energy"] = saved_energy;
+        list["Avoided_emissions"] = avoided_emissions;
+        list["Monthly_savings"] = monthly_savings;
+        list["Annual_savings"] = annual_savings;
+        list["Young_tree"] = young_tree;
+        list["Old_tree"] = old_tree;
+        list["Energy_H2_Cylinders"] = energy_H2_Cylinders;
+        list["Energy_H2_Low_Presure"] = energy_H2_Low_Presure;
+        list["Energy_consumed"] = energy_consumed;
+        list["Hydrogen_mass"] = hydrogen_mass;
+        list["Liters_required"] = liters_required;
+
+        response.success(req, res, list, 200);
     } catch (error) {
-        response.error(req, res, error.message, 500); 
+        response.error(req, res, error.message, 500);
     }
 })
- 
-module.exports = router ;
+
+module.exports = router;
--- database/dummyDataset.js
+++ database/dummyDataset.js
@@ -1,17 +1,38 @@
 const dummyDataset = () => {
     return data = {
-        "hydrogen_energy_destiny": 33,
-        "electrolysis_eficiency_factor": 0.7,
-        "compresor_eficiency_factor": 0.8,
-        "cell_fuel_eficiency_factor": 0.6,
-        "young_tree": 22,
-        "old_tree": 10,
+        "avg_speed": 50,
+        "autonomy_factor": 0.9,
+        "base_weight": 20000,
+        "base_km": 98550,
+        "compresor_eficiency_factor": 0.8,      //this is in env.js
+        "cell_fuel_eficiency_factor": 0.6,      //this is in env.js
         "checked_percentage": 0.05,
+        "cell_fuel_eficiency_factor": 0.54,
+        "compresor_eficiency_factor": 0.95,     //  this is in env.js
+        "combustion_engine_efficiency": 0.27,
+        "diesel_energy": 40.7,
+        "electrolysis_eficiency_factor": 0.76,  //this is in env.js
+        "emision_factor_gasoline": 69.25,
+        "emision_factor_diesel": 74.01,
+        "gasoline_energy": 35.58,
+        "hydrogen_energy_density": 33.33,  // this is in env.js
+        "km_checked": 200000,
+        "nox_reduction_factor": 0.9,
+        "old_tree": 30,                  // this is in env.js
+        "operation_factor": 0.9,
+        "percentage_consumption_savings": 0.1,
+        "percentage_urea_consumption": 0.07,
+        "water_h2_weight": 9,           // this is in env.js
+        "young_tree": 10,               // this is in env.js
         
-    }
-}
-
+        // defaults 4 calcs
+        "nominal_energy": 8.14,
+        "autonomy_nominal": 14.7,
+        "energy_price": 978.81,
+        "annual_use": 10000  // example annual use in km
+    };
+};
 
 module.exports = {
     dummyDataset
-}
+};
